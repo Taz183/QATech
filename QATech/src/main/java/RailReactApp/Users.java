e2c79eb6c6cbd5e60419a86f9a3c38be191bfaae
Naresh Mereddy <nareshmereddy@gmail.com>
2020-02-07 20:06:58 +0000

    minor update

--- QATech/src/main/java/RailReactApp/Users.java
+++ QATech/src/main/java/RailReactApp/Users.java
@@ -3,9 +3,6 @@ package RailReactApp;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
-
-import java.util.Date;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -17,7 +14,7 @@ public class Users {
 	String baseUri = "https://showoff-rails-react-production.herokuapp.com/";
 	String client_id = "277ef29692f9a70d511415dc60592daf4cf2c6f6552d3e1b769924b2f2e2e6fe";
 	String client_secret = "d6106f26e8ff5b749a606a1fba557f44eb3dca8f48596847770beb9b643ea352";
-	String token = "Bearer b530f9ad13a061b36aa342b255608e18960db09cfd977cab8c1de9a0f8226024";
+	String token = "Bearer 58d0522437a51e08ffef821f3194947f201c8fe8695731aad866960b4e961ea6";
 
 	@Test
 	public void CreateUser() {
@@ -48,7 +45,7 @@ public class Users {
 	public void ShowId() {
 		RestAssured.baseURI = baseUri;		
 		given().
-		header("Authorization", "Bearer 58d0522437a51e08ffef821f3194947f201c8fe8695731aad866960b4e961ea6").
+		header("Authorization", token).
 		when().
 		get("api/v1/users/4").
 		then().assertThat().statusCode(200).and().		
@@ -61,7 +58,7 @@ public class Users {
 		RestAssured.baseURI = baseUri;		
 		given().
 		header("Content-Type", "application/json").
-		header("Authorization", "Bearer 58d0522437a51e08ffef821f3194947f201c8fe8695731aad866960b4e961ea6").	
+		header("Authorization", token).	
 		body("{ "+
 				"\"user\" : { "+
 				"\"current_password\" : \"password\", "+
